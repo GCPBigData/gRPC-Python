e15c06669cbffaf69f249dfa47d6f2cfd10609cf
Jose RF Junior <web2ajax@gmail.com>
2020-07-06 22:36:35 -0400

    update grpc file transfer

---
+++ grpc-file-transfer/src/demo_client.py
@@ -0,0 +1,22 @@
+import os
+import lib
+
+if __name__ == '__main__':
+    client = lib.FileClient('localhost:8888')
+
+    # demo for file uploading
+    in_file_name = 'C:/Users/usuario/Documents/Dev/grpc-file-transfer/src/tmp/file.parquet'
+    print("FAZENDO UPLOADING DO ARQUIVO PARQUET")
+    client.upload(in_file_name)
+
+    #demo for file downloading:
+    out_file_name = 'C:/Users/usuario/Documents/Dev/grpc-file-transfer/src/tmp/'
+    if os.path.exists(out_file_name):
+       print("aqui 1")
+       os.remove(out_file_name)
+       print("aqui 2")
+    client.download('file.paquet', out_file_name)
+    print("aqui 3")
+    os.system(f'sha1sum {in_file_name}')
+    os.system(f'sha1sum{out_file_name}')
+    print("aqui 4")
---
+++ grpc-file-transfer/src/demo_server.py
@@ -0,0 +1,5 @@
+import lib
+
+if __name__ == '__main__':
+    print("INICIANDO SERVIDOR NA PORTA 8888")
+    lib.FileServer().start(8888)
