ef433910dcae2049844c995cc60228b7ff7fdc0a
Jose RF Junior <web2ajax@gmail.com>
2020-07-07 09:41:35 -0400

    update grpc file transfer

--- grpc-file-transfer/src/demo_client.py
+++ grpc-file-transfer/src/demo_client.py
@@ -2,7 +2,7 @@ import os
 import lib
 
 if __name__ == '__main__':
-    client = lib.FileClient('localhost:8888')
+    client = lib.FileClient('191.252.204.57:1000')
 
     # demo for file uploading
     #in_file_name = 'C:/teste/file2.parquet'
@@ -10,12 +10,12 @@ if __name__ == '__main__':
     #client.upload(in_file_name)
 
     #demo for file downloading:
-    out_file_name = 'D:/teste/baixado/file.mkv'
+    out_file_name = 'D:/teste/baixado/file.txt'
     if os.path.exists(out_file_name):
        os.remove(out_file_name)
 
     print(client)
-    client.download('file.mkv', out_file_name)
+    client.download('file.txt', out_file_name)
     #os.system(f'sha1sum {in_file_name}')
     #os.system(out_file_name)
     print("ARQUIVO BAIXADO NA PASTA :", out_file_name)
