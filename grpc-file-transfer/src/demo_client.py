f2184be5f9df4a80eb7d1bbc028aeaba9596757e
Jose RF Junior <web2ajax@gmail.com>
2020-07-06 23:37:48 -0400

    update grpc file transfer

--- grpc-file-transfer/src/demo_client.py
+++ grpc-file-transfer/src/demo_client.py
@@ -5,18 +5,18 @@ if __name__ == '__main__':
     client = lib.FileClient('localhost:8888')
 
     # demo for file uploading
-    in_file_name = 'C:/teste/file2.parquet'
-    print("FAZENDO UPLOADING DO ARQUIVO PARQUET")
-    client.upload(in_file_name)
+    #in_file_name = 'C:/teste/file2.parquet'
+    #print("FAZENDO UPLOADING DO ARQUIVO PARQUET")
+    #client.upload(in_file_name)
 
     #demo for file downloading:
-    out_file_name = 'C:/teste/file2.parquet'
+    out_file_name = 'C:/teste/baixados/'
     if os.path.exists(out_file_name):
        print("aqui 1")
        os.remove(out_file_name)
        print("aqui 2")
     client.download('file.paquet', out_file_name)
     print("aqui 3")
-    os.system(f'sha1sum {in_file_name}')
-    os.system(f'sha1sum{out_file_name}')
+    #os.system(f'sha1sum {in_file_name}')
+    os.system(out_file_name)
     print("aqui 4")
